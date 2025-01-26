59a5651664cafc1311eb9f63f0cebe5a7ff92abd
CpmJhon21 <cpmjho@gmail.com>
2025-01-26 15:27:01 +0700

    Bitly.js

--- Bitly.js
+++ Bitly.js
@@ -1,5 +1,4 @@
-// Bitly API Key
-const API_KEY_BITLY = "87bb1223a7e89887f5019e6254306d7cdff69cea"; // Ganti dengan API key Anda
+const API_KEY_BITLY = "87bb1223a7e89887f5019e6254306d7cdff69cea";
 
 export async function shortenWithBitly(longUrl) {
   const response = await fetch("https://api-ssl.bitly.com/v4/shorten", {
@@ -8,12 +7,11 @@ export async function shortenWithBitly(longUrl) {
       "Content-Type": "application/json",
       Authorization: `Bearer ${API_KEY_BITLY}`,
     },
-    body: JSON.stringify({ "long_url": "https://dev.bitly.com", "domain": "bit.ly", "group_guid": "Ba1bc23dE4F" })
-});
+    body: JSON.stringify({ long_url: longUrl }),
   });
 
   if (!response.ok) {
-    throw new Error(`Bitly API Error: ${response.status} ${response.statusText}`);
+    throw new Error("Bitly API Error");
   }
 
   const data = await response.json();
